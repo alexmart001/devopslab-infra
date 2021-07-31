dae929f4845d7aa0d275e03aa14d73a1cb7fb660
Alexandre Martins <alexandre.martins.001@gmail.com>
2021-07-31 13:18:56 -0300

    Terraform final

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "3.64.0"
+      source  = "hashicorp/google"
+      version = "3.77.0"
     }
   }
 }
